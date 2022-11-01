f670847a9bd11db6dc3fefc09bf4d0ea8d9cc6f4
Erik Presnov <erik.presnov160@gmail.com>
2022-11-01 19:24:25 +0200

    dropdown, improved json and post structure

--- js/script.js
+++ js/script.js
@@ -1,28 +1,73 @@
 window.onload = function () {
-  fetch("http://myjson.dit.upm.es/api/bins/7lc2")
+  fetch("http://myjson.dit.upm.es/api/bins/9jju")
+  //fetch('../json/myjson.json')
     .then((response) => response.json())
     .then((json) => {
       console.log(json);
+      let postDiv = document.querySelector(".posts");
       for (let i = 0; i < json.length; i++) {
-        let divElement = document.createElement("div");
-        let title = document.createElement("h3");
-        let postDiv = document.querySelector(".posts");
-        let paragraphElement = document.createElement("p");
-        let imageElement = document.createElement("img");
-        imageElement.src = json[i].image;
-        imageElement.width = 150;
-        imageElement.height = 150;
+        let authorImage = document.createElement('img');
+        authorImage.src = "../img/emptyProfile.png";
+        authorImage.width = 35;
+        authorImage.height = 35;
+        authorImage.classList.toggle('author');
 
-        divElement.className = "post color";
+        let date = document.createElement("p");
+        date.innerHTML = json[i].date;
+        date.classList.toggle("date");
 
+        let title = document.createElement('p');
         title.innerHTML = json[i].title;
-        paragraphElement.innerHTML = json[i].body;
+        title.classList.toggle('title');
 
-        divElement.appendChild(title);
-        divElement.appendChild(imageElement);
-        divElement.appendChild(paragraphElement);
+        let topDiv = document.createElement("div");
+        topDiv.classList.toggle('top_div');
+        topDiv.appendChild(authorImage);
+        topDiv.appendChild(title);
+        topDiv.appendChild(date);
 
-        postDiv.appendChild(divElement);
+
+        let image = document.createElement("img");
+        image.src = json[i].image;
+        image.width = 75;
+        image.height = 75;
+
+        let imgDiv = document.createElement("div");
+        imgDiv.appendChild(image);
+
+        let textDiv = document.createElement("div");
+        textDiv.innerHTML = json[i].body;
+
+        let contentDiv = document.createElement("div");
+        contentDiv.classList.toggle('content');
+        contentDiv.appendChild(textDiv);
+        contentDiv.appendChild(imgDiv);
+
+        let like = document.createElement("img");
+        like.classList.toggle('float_left');
+        like.src = "../img/like.png";
+        like.width = 30;
+        like.height = 30;
+
+        let dislike = document.createElement("img");
+        dislike.classList.toggle('float_left');
+        dislike.src = "../img/dislike.png";
+        dislike.width = 30;
+        dislike.height = 30;
+
+
+        let bottomDiv = document.createElement("div");
+        bottomDiv.classList.toggle('bottom_div');
+        bottomDiv.appendChild(like);
+        bottomDiv.appendChild(dislike);
+
+        let postBody = document.createElement("div");
+        postBody.classList.toggle('post')
+        postBody.appendChild(topDiv);
+        postBody.appendChild(contentDiv);
+        postBody.appendChild(bottomDiv);
+
+        postDiv.appendChild(postBody);
       }
     })
     .catch((err) => {
@@ -39,3 +84,20 @@ window.onload = function () {
         })
          */
 };
+
+function showInfo() {
+    document.getElementById("dropDown").classList.toggle("show");
+} 
+
+function hideInfo() {
+  var myDropdown = document.getElementById("dropDown");
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
+    }
+}
+
+window.onclick = function(e) {
+  if (!e.target.matches('.profilePic')) {
+    hideInfo();
+  }
+}
